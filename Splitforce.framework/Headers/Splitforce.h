ae4f95ccec1eb57f9d882bf7d4b7d1ace3ee2cd5
Stephen Price <steve@ikuramedia.com>
2014-06-04 18:00:29 +0800

    Update new framework structure

---
+++ Splitforce.framework/Headers/Splitforce.h
@@ -0,0 +1,19 @@
+//
+//  Splitforce.h
+//  Splitforce
+//
+//  Created by Stephen Price on 04/06/2014.
+//  Copyright (c) 2014 Ikura Group Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Splitforce.
+FOUNDATION_EXPORT double SplitforceVersionNumber;
+
+//! Project version string for Splitforce.
+FOUNDATION_EXPORT const unsigned char SplitforceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Splitforce/PublicHeader.h>
+
+#import <Splitforce/SFKit.h>
